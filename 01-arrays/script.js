8be36d7bf82c47517d4844909e5d1f25555952d9
FrancescoGiommi <francescogiommi97@icloud.com>
2024-10-16 15:08:27 +0200

    Add 1st point

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -1,16 +1,9 @@
-const teachers = [
-  'Nathan',
-  'Ed',
-  'Fabio',
-  'Phil',
-  'Carlo',
-  'Lewis',
-  'Luca'
-]; // NON MODIFICARE QUESTA VARIABILE
+const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; // NON MODIFICARE QUESTA VARIABILE
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = null;
+fourthTeacher = [3];
+console.log(teachers[3]);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
